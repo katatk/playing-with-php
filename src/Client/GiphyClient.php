8ebb7f24e4d95af0b1fef41d39f7c1ebc4902115
Kat <katatk@gmail.com>
2017-10-11 23:52:45 +0000

    set up php files

---
+++ src/Client/GiphyClient.php
@@ -0,0 +1,28 @@
+<?php
+namespace app\Client;
+
+use app\Model\Result;
+
+class GiphyClient
+{
+    private $apiKey = 'PwnmmMBaE92zhB6Q5twiONOphwaXoPsr';
+    public function test(): string
+    {
+        return 'giphy';
+    }
+    public function search(string $query): array
+    {
+        $params = "api_key={$this->apiKey}&q=$query&limit=5";
+        $url = "https://api.giphy.com/v1/gifs/search?$params";
+        $response = json_decode(file_get_contents($url), true);
+        $results = [];
+        
+        foreach ($response['data'] as $r) {
+            $res = new Result();
+            $res->url = $r['embed_url'];
+            $results[] = $res;
+        }
+
+        return $results;
+    }
+}
---
+++ src/Model/Result.php
@@ -0,0 +1,7 @@
+<?php
+namespace app\Model;
+
+class Result
+{
+    public $url;
+}
